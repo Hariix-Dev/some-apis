f87f4996dad8c6e7ef6357eb7ad268d72a24985d
Hariix_ <>
2019-05-25 15:18:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 /* jshint esversion: 6 */
 /* jshint -W032 */
 const express = require("express");
-const app = express()
+const app = express();
+
+const server = app.listen(process.env.PORT, () => {
+	const host = server.address().address;
+	const port = server.address().port;
+
+	console.log("Example app listening at http://%s:%s", host, port);
+});
